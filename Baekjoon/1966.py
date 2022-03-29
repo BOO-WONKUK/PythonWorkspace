eb8383e9e996e10b34b84f58673f28d71ea4d2c4
BOO-WONKUK <wkb0307@naver.com>
2022-03-29 21:54:19 +0900

    프린터 큐

--- Baekjoon/1966.py
+++ Baekjoon/1966.py
@@ -1,20 +1,26 @@
-import sys, heapq
+import sys
 
 n = int(sys.stdin.readline())
-h = []
 
 for _ in range(n):
-    a = int(sys.stdin.readline())
+    h = []
+    x, y = map(int, sys.stdin.readline().split())
+    lst = list(map(int, sys.stdin.readline().split()))
+    lstsub = [False for i in range(x)]
+    lstsub[y] = True
+    cnt = 0
 
-    if a != 0:
-        if a < 0:
-            heapq.heappush(h, (-a, a))
-        elif a > 0:
-            heapq.heappush(h, (a, a))
-        
-    else:
-        if len(h) == 0:
-            print("0")
-            continue
-        print(heapq.heappop(h)[1])
-        
+    while True:
+        if lst[0] == max(lst):
+            cnt += 1
+            if lstsub[0] == True:
+                print(cnt)
+                break
+            else:
+                del lst[0]
+                del lstsub[0]
+        else:
+            lst.append(lst[0])
+            lstsub.append(lstsub[0])
+            del lst[0]
+            del lstsub[0]
